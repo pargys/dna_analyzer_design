f406b68352f586d432c83ef232d76b87b529f772
pargys <ps@sem-wolf.org>
2020-07-05 18:31:18 -0400

    adds operator++ and operator<

--- src/id_dna.cpp
+++ src/id_dna.cpp
@@ -2,4 +2,11 @@
 // Created by y on 7/5/20.
 //
 #include "id_dna.h"
+IdDna& IdDna::operator++(){
+    ++m_id;
+    return *this;
+}
 
+bool operator<(const IdDna& id1, const IdDna& id2){
+    return id1.getId() < id2.getId();
+}
--- src/id_dna.h
+++ src/id_dna.h
@@ -8,9 +8,13 @@
 
 class IdDna{
 public:
+    IdDna(size_t id = 0):m_id(id){}
+    IdDna& operator++();
     size_t getId()const {return m_id;}
+
 private:
     size_t m_id;
 };
 
+bool operator<(const IdDna& id1, const IdDna& id2);
 #endif //SRC_ID_DNA_H
