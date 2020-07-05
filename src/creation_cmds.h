1e8d7716f21808c9389efe51381da181c732b0b8
pargys <ps@sem-wolf.org>
2020-07-05 07:59:59 -0400

    class decleration

---
+++ src/creation_cmds.h
@@ -0,0 +1,13 @@
+//
+// Created by y on 7/5/20.
+//
+
+#ifndef SRC_CREATION_CMDS_H
+#define SRC_CREATION_CMDS_H
+#include "ICommand.h"
+
+class CreationCmds: public ICommand{
+    /*virtual*/ void run() = 0;
+};
+
+#endif //SRC_CREATION_CMDS_H
