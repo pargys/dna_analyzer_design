55c2c93c8233e2e8078e75fd254a9077c72b5d76
pargys <ps@sem-wolf.org>
2020-07-05 13:51:19 -0400

    adds the option to read from the keyboard

---
+++ src/keyboard_reader.cpp
@@ -0,0 +1,10 @@
+//
+// Created by y on 6/30/20.
+//
+
+#include <iostream>
+#include "keyboard_reader.h"
+void KeyboardReader::read(){
+    std::cout<< "please enter str"<< std::endl;
+    std::cin>> m_str;
+}
---
+++ src/keyboard_reader.h
@@ -0,0 +1,19 @@
+//
+// Created by y on 6/30/20.
+//
+
+#ifndef DNA_SEQUENCE_KEYBOARD_READER_H
+#define DNA_SEQUENCE_KEYBOARD_READER_H
+#include <string>
+#include "ireader.h"
+
+class KeyboardReader: public IReader{
+public:
+    void read();
+    const std::string& getStr() const{return m_str;}
+
+private:
+    std::string m_str;
+};
+
+#endif //DNA_SEQUENCE_KEYBOARD_READER_H
