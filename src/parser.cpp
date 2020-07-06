9888e4c95132f9376605db4c79be56ddf4382f61
pargys <ps@sem-wolf.org>
2020-07-06 13:03:17 -0400

    correcting the method parseInput

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,8 +5,8 @@
 
 void Parser::parseInput(std::string input){
     size_t pos=0;
-    std::string param, delimiter=" ";
-
+    std::string param;
+    char delimiter=' ';
     if((pos=input.find(delimiter)) == std::string::npos){
         param = input.substr(0, pos);
         m_cmdName = param;
@@ -15,12 +15,12 @@ void Parser::parseInput(std::string input){
 
     param = input.substr(0, pos);
     m_cmdName = param;
-    input.erase(0, pos+delimiter.length());
+    input.erase(0, pos+1);
 
     while((pos=input.find(delimiter)) != std::string::npos){
         param = input.substr(0, pos);
         m_params.push_back(param);
-        input.erase(0, pos+delimiter.length());
+        input.erase(0, pos+1);
     }
-    m_params.push_back(param);
+    m_params.push_back(input);
 }
