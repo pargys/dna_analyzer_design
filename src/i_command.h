ef74ed56d80b6847e47854613fa928e01d739ef0
pargys <ps@sem-wolf.org>
2020-07-06 13:00:07 -0400

    adds the print method decleration

--- src/i_command.h
+++ src/i_command.h
@@ -7,10 +7,12 @@
 #include <vector>
 #include <string>
 #include "parser.h"
+#include "iwriter.h"
 
 class ICommand{
 public:
     virtual void run(const Parser& cmd) = 0;
+    virtual void print(IWriter* writer) = 0;
 };
 
 #endif //SRC_I_COMMAND_H
