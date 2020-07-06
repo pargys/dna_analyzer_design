fc5fb602c6d2a9fed545988c485ae9f546bdc663
pargys <ps@sem-wolf.org>
2020-07-06 13:21:49 -0400

    adds the option to write to the screen

---
+++ src/screen_writer.cpp
@@ -0,0 +1,9 @@
+//
+// Created by y on 6/30/20.
+//
+
+#include <iostream>
+#include "screen_writer.h"
+void ScreenWriter::write(std::string str)const{
+    std::cout<< str<< std::endl;
+}
---
+++ src/screen_writer.h
@@ -0,0 +1,13 @@
+//
+// Created by y on 6/30/20.
+//
+
+#ifndef DNA_SEQUENCE_SCREEN_WRITER_H
+#define DNA_SEQUENCE_SCREEN_WRITER_H
+#include "iwriter.h"
+class ScreenWriter: public IWriter{
+public:
+    /*virtual*/void write(std::string str)const;
+};
+
+#endif //DNA_SEQUENCE_SCREEN_WRITER_H
