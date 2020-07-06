070a88ecf6d40fad58dc70cc9f9627c456eb5b91
pargys <ps@sem-wolf.org>
2020-07-06 12:53:26 -0400

    add operator<

--- src/name_dna.h
+++ src/name_dna.h
@@ -9,10 +9,12 @@
 class NameDna{
 public:
     NameDna(const std::string& name = ""):m_name(name){}
-    const std::string& getDna()const {return m_name;}
+    const std::string& getName()const {return m_name;}
 
 private:
     std::string m_name;
 };
+
+bool operator<(const NameDna& name1, const NameDna& name2);
 
 #endif //SRC_NAME_DNA_H
