795c983d287587e757ebd32cdffd91384a0bf01c
pargys <ps@sem-wolf.org>
2020-07-07 13:32:10 -0400

    remove the print method from the class and update files inclusion

--- src/i_command.h
+++ src/i_command.h
@@ -4,15 +4,14 @@
 
 #ifndef SRC_I_COMMAND_H
 #define SRC_I_COMMAND_H
-#include <vector>
-#include <string>
-#include "parser.h"
-#include "iwriter.h"
+
+class Parser;
+class StructureDna;
+class IWriter;
 
 class ICommand{
 public:
-    virtual void run(const Parser& cmd) = 0;
-    virtual void print(IWriter* writer) = 0;
+    virtual void run(const Parser& cmd, StructureDna& structure, IWriter& output) = 0;
 };
 
 #endif //SRC_I_COMMAND_H
