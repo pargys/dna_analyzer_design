84d0d37395bc58a3bc3c4d2aea492e0ec508f592
pargys <ps@sem-wolf.org>
2020-07-07 13:21:22 -0400

    transfer the print method to be a class member and update the maps to be class members

--- src/new_cmd.cpp
+++ src/new_cmd.cpp
@@ -2,33 +2,35 @@
 // Created by y on 7/5/20.
 //
 
+#include "parser.h"
 #include "new_cmd.h"
 #include "structure_dna.h"
 #include <sstream>
+#include "iwriter.h"
+
 NewCmd::NewCmd(const Parser& cmd){
     if(cmd.getParams().size() < 1 || cmd.getParams().size() > 2){
         throw std::invalid_argument("invalid nums of arguments!");
     }
 }
 
-void NewCmd::run(const Parser& cmd){
+void NewCmd::run(const Parser& cmd, StructureDna& structure, IWriter& output) {
     std::string dnaName;
     std::stringstream ss;
-    if(cmd.getParams().size() == 1){
-        ss<< "seq"<< ++countDna;
+    if (cmd.getParams().size() == 1) {
+        ss << "seq" << ++countDna;
         dnaName = ss.str();
-    }
-    else{
+    } else {
         dnaName = cmd.getParams()[1];
     }
-    MetaDataDna* data = new MetaDataDna(cmd.getParams()[0], dnaName, (std::string)"new");
-    StructureDna::getIdStructure().insert(std::pair<IdDna, MetaDataDna*> (MetaDataDna::getId(), data));
-    StructureDna::getNameStructure().insert(std::pair<NameDna, MetaDataDna*>(dnaName, data));
+    MetaDataDna *data = new MetaDataDna(cmd.getParams()[0], dnaName, (std::string) "new");
+    structure.add(data);
+    print(structure, output);
 }
 
-void NewCmd::print(IWriter* writer){
+
+void NewCmd::print(StructureDna& structure, IWriter& output){
     std::stringstream idString;
-    idString<< StructureDna::getIdStructure()[MetaDataDna::getId()]->getId().getId();
-    std::string s = "[" + idString.str() + "]" + " " + StructureDna::getIdStructure()[MetaDataDna::getId()]->getName().getName() + ": " + StructureDna::getIdStructure()[MetaDataDna::getId()]->getDnaSeq().getDna() + "\n";
-    writer->write(s);
+    idString<< structure.getIdStructure()[MetaDataDna::getId()]->getId().getId();
+    output.write("[" + idString.str() + "]" + " " + structure.getIdStructure()[MetaDataDna::getId()]->getName().getName() + ": " + structure.getIdStructure()[MetaDataDna::getId()]->getDnaSeq().getDna() + "\n");
 }
--- src/new_cmd.h
+++ src/new_cmd.h
@@ -4,13 +4,16 @@
 
 #ifndef SRC_NEW_CMD_H
 #define SRC_NEW_CMD_H
+
 #include "creation_cmds.h"
 
 class NewCmd: public CreationCmds{
 public:
     NewCmd(const Parser& cmd);
-    /*virtual*/void run(const Parser& cmd);
-    /*virtual*/void print(IWriter* writer);
+    /*virtual*/void run(const Parser& cmd, StructureDna& structure, IWriter& output);
+
+private:
+    void print(StructureDna& structure, IWriter& output);
 };
 
 #endif //SRC_NEW_CMD_H
