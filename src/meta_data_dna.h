3508910225a7ad7c9585091a3092dd1a9f7b95d8
pargys <ps@sem-wolf.org>
2020-07-08 07:49:04 -0400

    add count member, getCounter and increaseCounter methods

--- src/meta_data_dna.h
+++ src/meta_data_dna.h
@@ -12,17 +12,20 @@
 
 class MetaDataDna{
 public:
-    MetaDataDna(const DnaSequence& dna, const NameDna& name, const StatusDna& status): m_dna(new DnaSequence(dna)), m_name(name), m_status(status){++s_id;}
+    MetaDataDna(const DnaSequence& dna, const NameDna& name, const StatusDna& status): m_dna(new DnaSequence(dna)), m_name(name), m_status(status), m_counter(0){++s_id;}
     ~MetaDataDna(){delete m_dna;}
     static IdDna getId(){return s_id;}
     const NameDna& getName()const {return m_name;}
     const DnaSequence& getDnaSeq()const {return *m_dna;}
+    size_t getCounter()const {return m_counter;}
+    void increaseCounter(){++m_counter;}
 
 private:
     DnaSequence* m_dna;
     NameDna m_name;
     StatusDna m_status;
     static IdDna s_id;
+    size_t m_counter;
 };
 
 #endif //SRC_META_DATA_DNA_H
