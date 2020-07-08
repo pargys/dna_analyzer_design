8aabd29658b28700de226a11737d18a98a0ae112
pargys <ps@sem-wolf.org>
2020-07-08 03:55:01 -0400

    change the m_dna member to be a pointer and add a dtor

--- src/meta_data_dna.h
+++ src/meta_data_dna.h
@@ -4,20 +4,22 @@
 
 #ifndef SRC_META_DATA_DNA_H
 #define SRC_META_DATA_DNA_H
+
 #include "dna_sequence.h"
 #include "name_dna.h"
 #include "id_dna.h"
 #include "status_dna.h"
-#include "id_dna.h"
+
 class MetaDataDna{
 public:
-    MetaDataDna(const DnaSequence& dna, const NameDna& name, const StatusDna& status): m_dna(dna), m_name(name), m_status(status){++s_id;}
+    MetaDataDna(const DnaSequence& dna, const NameDna& name, const StatusDna& status): m_dna(new DnaSequence(dna)), m_name(name), m_status(status){++s_id;}
+    ~MetaDataDna(){delete m_dna;}
     static IdDna getId(){return s_id;}
     const NameDna& getName()const {return m_name;}
-    const DnaSequence& getDnaSeq()const {return m_dna;}
+    const DnaSequence& getDnaSeq()const {return *m_dna;}
 
 private:
-    DnaSequence m_dna;
+    DnaSequence* m_dna;
     NameDna m_name;
     StatusDna m_status;
     static IdDna s_id;
